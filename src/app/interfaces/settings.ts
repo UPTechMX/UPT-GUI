270898b0814fd24942d1ac8b67dd6860f9f945cc
amilcar <52044033+Terminal-Redux@users.noreply.github.com>
2022-05-30 13:46:29 -0500

    Test

--- src/app/interfaces/settings.ts
+++ src/app/interfaces/settings.ts
@@ -1,10 +1,11 @@
 export interface Settings {
-    st_layer_id?: string;
-    id?: number;
-    label?: string;
-    normalization_method?: number;
-    range_min?: number;
-    range_max?: number;
-    weight?: number;
-    smaller_better?: number;
+  st_layer_id?: string;
+  st_public_layer_id?: string;
+  id?: number;
+  label?: string;
+  normalization_method?: number;
+  range_min?: number;
+  range_max?: number;
+  weight?: number;
+  smaller_better?: number;
 }
